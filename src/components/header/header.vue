427e5d317648ed63792194081c121a80ce7edd56
leesay <leesayhi@126.com>
2017-10-16 17:45:38 +0800

    star component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,24 +18,62 @@
 						<span class="text">{{seller.supports[0].description}}</span>
 					</div>
 				</div>
-				<div v-if="seller.supports" class="support-count">
+				<div v-if="seller.supports" class="support-count" @click="showDetail">
 					<span class="count">{{seller.supports.length}}个</span>
 					<i class="icon-keyboard_arrow_right"></i>
 				</div>
 			</div>
+      <div class="bulletin-wrapper" @click="showDetail">
+        <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+      <div class="background">
+        <img :src="seller.avatar" width="100%" height="100%">
+      </div>
+      <transition name="fade">
+        <div v-show="detailShow" class="detail" @click="detailClose">
+        <div class="detail-wrapper">
+          <div class="detail-main">
+            <h1 class="title">{{seller.name}}</h1>
+            <div class="star-wreapper">
+              <v-star :size="48" :score="seller.score"></v-star>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close"></div>
+        </div>
+      </transition>
 		</div>
 	</div>
 </template>
 
 <script>
+  import star from '../star/star.vue'
+
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
+      },
+      detailClose () {
+        this.detailShow = false
+      }
+    },
+    components: {
+      'v-star': star
     }
   }
 </script>
@@ -119,4 +157,55 @@
           margin-left: 2px
           line-height: 24px
           font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      padding: 0 22px 0 12px
+      height: 28px
+      line-height: 28px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        width: 22px
+        height: 12px
+        margin-top: 8px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        top: 8px
+        font-size: 10px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%;
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      transition: all 0.5s
+      backdrop-filter: blur(10px)
+      background: rgba(7, 17, 27, 0.8)
+      &.fade-enter-active, .fade-leave-active
+        opacity: 1
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter, &.fade-leave-to
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
 </style>
